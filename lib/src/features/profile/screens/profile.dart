9d7c4407aa3508dde54597b4af6741fde6a8d1d6
Hajra <hshahzad.bscs20seecs@seecs.edu.pk>
2024-03-10 15:21:01 +0500

    Fixed over flow error, updated profile page

--- lib/src/features/profile/screens/profile.dart
+++ lib/src/features/profile/screens/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../constants/constants.dart';
 import '../../../common widgets/custom_app_bar.dart';
+import '../../../common widgets/profile_information_container.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -8,67 +9,90 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Container(
-      width: MediaQuery.of(context).size.width,
-      color: kWhite,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 70,
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-              color: kBackgroundGrey,
-            ),
-          ),
-          Text(
-            'User\'s Name',
-            style: TextStyle(
-              color: kBlack,
-            ),
-          ),
-          Text('Personal Information', style: kH3Black,),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: kBabyPink,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        color: kWhite,
+        child: Padding(
+          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.06,),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 75,
+              ),
+              Stack(
+                clipBehavior: Clip.none,
                 children: [
-                  Text('Email: abcd@gmail.com', style: kNormalBarbiePink,),
-                  Text('Phone Number: 92 XXX XXXXXX', style: kNormalBarbiePink,),
-                  Text('Location: ..', style: kNormalBarbiePink,),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.88,
+                    height: 170,
+                    decoration: BoxDecoration(
+                      color: kBlack,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  Positioned(
+                      top: -45,
+                      left: 25,
+                      child: Container(
+                        width: 106,
+                        height: 106,
+                        decoration: BoxDecoration(
+                            color: kBarbiePink,
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/user_profile.png'),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                            border: Border.all(
+                              color: kBarbiePink,
+                              width: 2.0,
+                            )),
+                      ))
                 ],
               ),
-            ),
-          ),
-          Text('Skin Tone Related Information', style: kH3Black,),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: kBabyPink,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Skin Tone: 6', style: kNormalBarbiePink,),
-                  Text('Undertone: warm', style: kNormalBarbiePink,),
-                  Text('Recommended Foundations: ..', style: kNormalBarbiePink,),
-                ],
+              SizedBox(
+                height: 20,
               ),
-            ),
+              Text(
+                'Personal Information',
+                style: kRegularBlackH1,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.88,
+                decoration: BoxDecoration(
+                  color: kBackgroundGrey,
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
+                child: Column(
+                  children: [
+                    ProfileInformationContainer(fieldTitle: 'Name', value: 'Hajra Shahzad', icon: Icons.person,), //Information from db
+                    ProfileInformationContainer(fieldTitle: 'Email Address', value: 'hajrashahzad24@gmail.com', icon: Icons.email,) //TODO: fix text overflow here
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Settings',
+                style: kRegularBlackH1,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.88,
+                height: 300,
+                decoration: BoxDecoration(
+                  color: kBackgroundGrey,
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     ));
   }
 }
+
+
